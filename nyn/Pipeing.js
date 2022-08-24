c8890ecfc1996334f6da2dcd2d9beced9c9a9476
nayan patidar <patidarnayan86@gmail.com>
2022-08-24 12:43:58 +0530

    change success

--- nyn/Pipeing.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs");
-
-var readerStream = fs.createReadStream('example.txt');
-
-var writerStream = fs.createWriteStream('example1.txt');
-
-readerStream.pipe(writerStream);
-
-console.log("Program Ended");
