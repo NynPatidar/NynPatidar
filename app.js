c8e2c84159446158515d5fc3cc31154c44ec5a33
nayan patidar <patidarnayan86@gmail.com>
2022-08-30 11:10:32 +0530

    meessage

--- app.js
+++ app.js
@@ -4,27 +4,27 @@ const app = express();
 const hbs = require("hbs");
 
 
-require("./db/conn");
-const Register = require("./models/registers");
+require("./src/db/conn");
+const Register = require("./src/models/registers");
 const { json } = require("express");
 
 const port = process.env.PORT || 3000;
 
-const static_path = path.join(__dirname, "../public");
-const template_path = path.join(__dirname, "../templates/views");
-const partials_path = path.join(__dirname, "../templates/partials");
+// const static_path = path.join(__dirname, "../");
+const view_path = path.join(__dirname, "../Nodejs/templates/views");
+const partials_path = path.join(__dirname, "../Nodejs/templates/partials");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false})); 
 
-app.use(express.static(static_path));
+// app.use(express.static(static_path));
 app.set("view engine", "hbs");
 
-app.set("views", template_path);
+app.set("views", view_path);
 hbs.registerPartials(partials_path);
 
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("index");
 });
 
 app.get("/register", (req, res) => {
