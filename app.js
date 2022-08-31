eedf000238bcac6f8b77441454e322f8668fee2a
nayan patidar <patidarnayan86@gmail.com>
2022-08-31 13:24:55 +0530

    file added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const hbs = require("hbs");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ||'3000';
 
 require("./src/db/conn");
 const Register = require("./src/models/registers");
